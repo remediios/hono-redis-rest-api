4b211cfde852d8d9cc980ce932d436b959f7cb7e
Miguel Remédios <miguelremediios@gmail.com>
2024-04-08 21:10:00 +0100

    feat: shadcnui setup and base page.tsx implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,15 +19,24 @@ export default function Home() {
   }, [input]);
 
   return (
-    <div>
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => {
-          setInput(e.target.value);
-        }}
-        className="text-zinc-900"
-      />
-    </div>
+    <main className="h-screen w-screen grainy">
+      <div className="flex flex-col gap-6 items-center pt-32 duration-500 animate-in animate fade-in slide-in-from-bottom-2.5">
+        <h1 className="text-5xl tracking-tight font-bold">Hono Redis API</h1>
+        <p className="text-zinc-600 text-md max-w-prose text-center">
+          Globally distributed high-performance REST API for speed search
+          <br /> Type a query below and get your results in miliseconds.
+        </p>
+
+        <div className="max-w-md w-full"></div>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+          className="text-zinc-900"
+        />
+      </div>
+    </main>
   );
 }
