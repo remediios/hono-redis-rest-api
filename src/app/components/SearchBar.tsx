2743fdbe2d00ba4b5fb058fbdb8938125a21b191
Miguel Remédios <miguelremediios@gmail.com>
2024-04-09 19:04:21 +0100

    fix: selecteResult logic implementation

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   Command,
   CommandEmpty,
@@ -6,6 +8,7 @@ import {
   CommandList,
   CommandGroup,
 } from '@/components/ui/command';
+import { useState } from 'react';
 
 interface ISearchBar {
   input: string;
@@ -19,17 +22,21 @@ interface ISearchBar {
 }
 
 const SearchBar = ({ input, setInput, searchResults }: ISearchBar) => {
+  const [selectedResult, setSelectedResult] = useState(false);
   return (
     <div className="max-w-md w-full ">
       <Command className="rounded-xl border shadow-md">
         <CommandInput
           value={input}
-          onValueChange={(e) => setInput(e)}
+          onValueChange={(e) => {
+            setInput(e);
+            setSelectedResult(false);
+          }}
           placeholder="Search countries..."
           className="placeholder:text-zinc-500"
         />
         <CommandList>
-          {searchResults?.results.length === 0 ? (
+          {searchResults?.results.length === 0 && !selectedResult ? (
             <CommandEmpty>No results found.</CommandEmpty>
           ) : null}
           {searchResults?.results ? (
@@ -38,14 +45,17 @@ const SearchBar = ({ input, setInput, searchResults }: ISearchBar) => {
                 <CommandItem
                   key={index}
                   value={result}
-                  onSelect={(e) => setInput(e)}
+                  onSelect={(e) => {
+                    setInput(e);
+                    setSelectedResult(true);
+                  }}
                 >
                   <p>{result}</p>
                 </CommandItem>
               ))}
             </CommandGroup>
           ) : null}
-          {searchResults?.results ? (
+          {searchResults?.results && !selectedResult ? (
             <>
               <div className="h-px w-full bg-zinc-200" />
               <p className="p-2 text-xs text-zinc-500">
