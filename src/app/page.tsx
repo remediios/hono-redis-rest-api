4d118bc0a2944ef88ca4287112e8911b2968e951
Miguel Remédios <miguelremediios@gmail.com>
2024-04-10 19:48:34 +0100

    fix: client fetching request API url fixing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       if (!input) return setSearchResults(undefined);
 
       const response = await fetch(
-        `http://localhost:3000/api/search?q=${input}`
+        `https://hono-redis-rest-api.remedios.workers.dev/api/search?q=${input}`
       );
       const data = (await response.json()) as {
         results: string[];
